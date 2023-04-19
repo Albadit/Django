63c4e28ee01c5655fb45fe5282969bd61a7e9eeb
Albadit <46565849+Albadit@users.noreply.github.com>
2023-04-19 20:02:12 +0200

    clean

--- base/models.py
+++ base/models.py
@@ -3,8 +3,6 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-
-
 # Create your models here.
 class Profile(models.Model):
     location = models.CharField(max_length=100)
@@ -13,7 +11,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     def __str__(self):
         return self.user.username
-    
+
 class Book(models.Model):
     Title = models.CharField(max_length=100)
     Author = models.CharField(max_length=100)
@@ -24,14 +22,14 @@ class Book(models.Model):
                                     related_name='approved_by', null=True, blank=True)
     def __str__(self):
         return self.user.username
-    
+
 class Read(models.Model):
     SCORE_CHOICES = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     Book = models.ForeignKey(Book, on_delete=models.CASCADE)
     User = models.ForeignKey(Profile, on_delete=models.CASCADE)
     Date = models.DateField(null=False, blank=False)
     Score = models.IntegerField(null=False, blank=False)
-    
+
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
